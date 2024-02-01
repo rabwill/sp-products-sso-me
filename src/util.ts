22e48b40677367f19795f503d218eaef0905de25
Rabia Williams <rabiawilliams@gmail.com>
2024-02-01 20:50:24 +1000

    Fixed the AC error

--- src/util.ts
+++ src/util.ts
@@ -3,18 +3,14 @@ import { AdaptiveCardInvokeResponse, InvokeResponse } from "botbuilder";
 export const CreateInvokeResponse = (status: number, body?: unknown): InvokeResponse => {
     return { status, body };
 };
-export const CreateAdaptiveCardInvokeResponse = (status: number, body?: any): AdaptiveCardInvokeResponse => {
-    return {
-        statusCode: status,
-        type: 'application/vnd.microsoft.card.adaptive',
-        value: { body }
-    };
+export const CreateAdaptiveCardInvokeResponse = (statusCode: number, body?: Record<string, unknown>): AdaptiveCardInvokeResponse => {
+   return {
+            statusCode: statusCode,
+            type: 'application/vnd.microsoft.card.adaptive',
+            value: body
+        };
 };
-export const CreateActionErrorResponse = (
-    statusCode: number,
-    errorCode: number = -1,
-    errorMessage: string = 'Unknown error'
-): AdaptiveCardInvokeResponse => {
+export const CreateActionErrorResponse = ( statusCode: number, errorCode: number = -1,    errorMessage: string = 'Unknown error'): AdaptiveCardInvokeResponse => {
     return {
         statusCode: statusCode,
         type: 'application/vnd.microsoft.error',
